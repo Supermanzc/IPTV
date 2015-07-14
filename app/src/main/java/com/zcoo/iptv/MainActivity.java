1d7d257c01d64e68a90988e85db204f241512bfd
zhong.chuan <zhgchuan@163.com>
2015-07-14 17:39:20 +0800

    LogUtil日志展示和常量创建

--- app/src/main/java/com/zcoo/iptv/MainActivity.java
+++ app/src/main/java/com/zcoo/iptv/MainActivity.java
@@ -3,6 +3,8 @@ package com.zcoo.iptv;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import com.zcoo.iptv.utils.LogUtil;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogUtil.e("我好想你，非常想你，真的很想你");
     }
 }
---
+++ app/src/main/java/com/zcoo/iptv/utils/LogUtil.java
@@ -0,0 +1,113 @@
+package com.zcoo.iptv.utils;
+
+import com.zcoo.iptv.config.Config;
+
+/**
+ * Author: Supermanzc
+ * Projectname: IPTV
+ * Packagename: com.zcoo.iptv.utils
+ * Time: 2015/7/14 17:07
+ */
+public class LogUtil {
+    public static final boolean isDebug = Config.DEBUG_MODE;
+
+    public static final String TAG = Config.DEBUG_TAG;
+
+    private static String getFunctionName() {
+        StackTraceElement[] sts = Thread.currentThread().getStackTrace();
+        if(sts == null){
+            return null;
+        }
+        for (StackTraceElement st : sts){
+            if(st.isNativeMethod()){
+                continue;
+            }
+            if (st.getClassName().equals(Thread.class.getName())) {
+                continue;
+            }
+            if(st.getClassName().equals(LogUtil.class.getName())){
+                continue;
+            }
+
+            return "[" + Thread.currentThread().getName() + "(" + Thread.currentThread().getId()
+                    + "): " + st.getFileName() + ":" + st.getLineNumber() + "]";
+        }
+        return null;
+    }
+
+    private static String createMessage(String msg){
+        String functionName = getFunctionName();
+        String message = (functionName == null ? msg : (functionName + " - " + msg));
+        return message;
+    }
+
+    public static void v(String msg) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.v(TAG, message);
+        }
+    }
+
+    public static void v(String msg, Throwable t) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.v(TAG, message, t);
+        }
+    }
+
+    public static void d(String msg) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.d(TAG, message);
+        }
+    }
+
+    public static void d(String msg, Throwable t) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.d(TAG, message, t);
+        }
+    }
+
+    public static void i(String msg) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.i(TAG, message);
+        }
+    }
+
+    public static void i(String msg, Throwable t) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.i(TAG, message, t);
+        }
+    }
+
+    public static void w(String msg) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.w(TAG, message);
+        }
+    }
+
+    public static void w(String msg, Throwable t) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.w(TAG, message, t);
+        }
+    }
+
+    public static void e(String msg) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.e(TAG, message);
+        }
+    }
+
+    public static void e(String msg, Throwable t) {
+        if (isDebug) {
+            String message = createMessage(msg);
+            android.util.Log.e(TAG, message, t);
+        }
+    }
+}
